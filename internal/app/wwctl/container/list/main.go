0e2c5d92a59582bad36b2d693879a8d4d0f92086
MatthewHink <matthew_hink@yahoo.com>
2022-02-17 09:55:55 -0500

    wwapi preview

--- internal/app/wwctl/container/list/main.go
+++ internal/app/wwctl/container/list/main.go
@@ -2,40 +2,26 @@ package list
 
 import (
 	"fmt"
-	"os"
 
-	"github.com/hpcng/warewulf/internal/pkg/container"
-	"github.com/hpcng/warewulf/internal/pkg/node"
-	"github.com/hpcng/warewulf/internal/pkg/util"
+	"github.com/hpcng/warewulf/internal/pkg/api/container"
 	"github.com/hpcng/warewulf/internal/pkg/wwlog"
 	"github.com/spf13/cobra"
 )
 
-func CobraRunE(cmd *cobra.Command, args []string) error {
+func CobraRunE(cmd *cobra.Command, args []string) (err error) {
 
-	sources, err := container.ListSources()
+	containerInfo, err := container.ContainerList()
 	if err != nil {
 		wwlog.Printf(wwlog.ERROR, "%s\n", err)
-		os.Exit(1)
-	}
-
-	nodeDB, _ := node.New()
-	nodes, _ := nodeDB.FindAllNodes()
-	nodemap := make(map[string]int)
-
-	for _, n := range nodes {
-		nodemap[n.ContainerName.Get()]++
+		return
 	}
 
 	fmt.Printf("%-35s %-6s %-6s\n", "CONTAINER NAME", "BUILT", "NODES")
-	for _, source := range sources {
-		image := container.ImageFile(source)
-
-		if nodemap[source] == 0 {
-			nodemap[source] = 0
-		}
-		fmt.Printf("%-35s %-6t %-6d\n", source, util.IsFile(image), nodemap[source])
-
+	for i := 0; i < len(containerInfo); i++ {
+		fmt.Printf("%-35s %-6t %-6d\n",
+			containerInfo[i].Name,
+			containerInfo[i].Built,
+			containerInfo[i].NodeCount)
 	}
-	return nil
+	return
 }

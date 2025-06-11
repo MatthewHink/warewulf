6daa190ef3c806ba0ade7cebfd2afbdef395740c
MatthewHink <matthew_hink@yahoo.com>
2025-06-11 10:35:11 -0700

    .

--- internal/pkg/overlay/overlay.go
+++ internal/pkg/overlay/overlay.go
@@ -564,10 +564,15 @@ type FileInfo struct {
 // Get all the files as a FileInfo slice for a given overlay.
 func OverlayGetFiles(name string) (files []FileInfo, err error) {
 	baseDir := GetOverlay(name).Rootfs()
+	wwlog.Warn("baseDir %s", baseDir) // TODO: Log level
 	if !util.IsDir(baseDir) {
+		wwlog.Warn("overlay %s doesn't exist", name) // TODO: Log level
 		err = fmt.Errorf("overlay %s doesn't exist", name)
 		return
 	}
+	//wwlog.Warn("os.Stat path %s", path) // TODO: Log level
+	//wwlog.Warn("os.Stat info %s", info) // TODO: Log level
+	//wwlog.Warn("os.Stat err %s", err)   // TODO: Log level
 	err = filepath.Walk(baseDir, func(path string, info fs.FileInfo, err error) error {
 		wwlog.Warn("os.Stat path %s", path) // TODO: Log level
 		wwlog.Warn("os.Stat info %s", info) // TODO: Log level
@@ -577,7 +582,10 @@ func OverlayGetFiles(name string) (files []FileInfo, err error) {
 
 			s, err := os.Stat(path)
 			if err != nil {
-				wwlog.Error("os.Stat error %s: %s: %w", name, path, err)
+				//wwlog.Error("os.Stat error %s: %s: %w", name, path, err)
+				wwlog.Warn("os.Stat path %s", path) // TODO: Log level
+				wwlog.Warn("os.Stat info %s", info) // TODO: Log level
+				wwlog.Warn("os.Stat err %s", err)   // TODO: Log level
 				// error is logged and continue, get what we can
 				// return???
 				return nil
